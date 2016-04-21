fe834ac51700183ef9b04164f55316b7c40739f9
Mac <mac@wintermute.local>
2016-04-21 03:10:56 -0300

    1st commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var fs = require('fs'),
+    youtube = require('youtube-dl');
+
+fs.readFile('videos.txt', 'utf-8', function(err, data) {
+  if(err) {
+    console.log('Impossible to open/read the file. ['+err+']');
+    return;
+  }
+
+  var videos = data.split('\n');
+  var ffmpegList = ''; // Create a reference file for ffmpeg concatenate
+
+  videos.forEach(function(url) {
+    if (url.length > 'https://youtube.com/'.length) {
+      var video = youtube(url);
+      var d = new Date();
+
+      video.on('info', function(info) {
+        console.log('Downloading' + info._filename + '...\n');
+        video.pipe(fs.createWriteStream('./tmp/'+d.getTime()+'.mp4'));
+      });
+    }
+  });
+});
